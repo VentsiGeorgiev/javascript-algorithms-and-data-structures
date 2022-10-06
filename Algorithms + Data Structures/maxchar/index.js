43f75a1dc92ec771a0da733fa2b6b7e78c43ddb7
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-06 16:57:02 +0200

    Added solution for maxchar

--- Algorithms + Data Structures/maxchar/index.js
+++ Algorithms + Data Structures/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const allChars = {};
+    let maxCounter = 0;
+    let maxChar = '';
+
+    for (const char of str) {
+        if (allChars[char]) {
+            allChars[char] += 1;
+        } else {
+            allChars[char] = 1;
+        }
+    }
+    for (const key in allChars) {
+        if (allChars[key] > maxCounter) {
+            maxChar = key;
+            maxCounter = allChars[key];
+        }
+    }
+    return maxChar;
+
+}
+console.log(maxChar('abcccccccd'));
+
 
 module.exports = maxChar;
