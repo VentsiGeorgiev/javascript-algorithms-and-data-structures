75fef2c6367ba0ae2312d7d9ad2ff27834058548
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-13 09:16:54 +0200

    Added solution for pyramid

--- Algorithms + Data Structures/pyramid/index.js
+++ Algorithms + Data Structures/pyramid/index.js
@@ -14,6 +14,29 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// n = row = 3;
+// col = (n * 2 - 1) 
+
+// let columns = [0,1,2,3,4];
+// let row = 0;
+// let center = Math.floor((2 * n - 1) / 2)
+
+function pyramid(n) {
+    let midpoint = Math.floor((2 * n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        let result = '';
+        for (let col = 0; col < 2 * n - 1; col++) {
+            if (midpoint - row <= col && midpoint + row >= col) {
+                result += '#';
+            } else {
+                result += ' ';
+            }
+        }
+        console.log(result);
+    }
+}
+
+pyramid(32);
 
 module.exports = pyramid;
