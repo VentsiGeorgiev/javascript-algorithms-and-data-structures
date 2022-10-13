647ea5c04eaa9f7a03cb9e6359bbcb043bb8374e
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-13 10:22:41 +0200

    Added solution for vowels

--- Algorithms + Data Structures/vowels/index.js
+++ Algorithms + Data Structures/vowels/index.js
@@ -7,6 +7,41 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+
+    let matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+
+
+    // let count = 0;
+    // let vowelsObj = {
+    //     a: 0,
+    //     e: 0,
+    //     i: 0,
+    //     o: 0,
+    //     u: 0
+    // };
+
+    // for (let i = 0; i < str.length; i++) {
+    //     let char = str[i].toLowerCase();
+    //     if (vowelsObj.hasOwnProperty(char)) {
+    //         count++;
+    //     }
+    // }
+
+    // console.log(count);
+
+    // let counter = 0;
+    // const checker = ['a', 'e', 'i', 'o', 'u'];
+
+    // for (let char of str.toLowerCase()) {
+    //     if (checker.includes(char)) {
+    //         counter++;
+    //     }
+    // }
+    // console.log(counter);
+
+}
+console.log(vowels('Hi There!'));
 
 module.exports = vowels;
