16c712a4472e95d32d5fb1f00d9af946a7d11b0e
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-14 21:18:48 +0200

    Added memoization for fib solution

--- Algorithms + Data Structures/fib/index.js
+++ Algorithms + Data Structures/fib/index.js
@@ -8,7 +8,22 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+function slowFib(n) {
     if (n < 2) {
         return n;
     }
@@ -17,7 +32,17 @@ function fib(n) {
 
 }
 
-console.log(fib(6));
+const fib = memoize(slowFib);
+// function fib(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+
+//     return fib(n - 1) + fib(n - 2);
+
+// }
+
+// console.log(fib(6));
 
 // function fib(n) {
 //     const result = [0, 1];
