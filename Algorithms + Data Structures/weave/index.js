a6cd804bc6e8ceecc4b7823244df40293fdbb58d
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-15 14:57:19 +0200

    Added solution for weave

--- Algorithms + Data Structures/weave/index.js
+++ Algorithms + Data Structures/weave/index.js
@@ -24,6 +24,19 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+    const q = new Queue();
+
+    while (sourceOne.peek() || sourceTwo.peak()) {
+        if (sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+
+        if (sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+
+    }
+}
 
 module.exports = weave;
